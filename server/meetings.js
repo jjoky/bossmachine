10181023ff224fb66c33287515638011b21b05a5
Jacques Joky <jacques.joky@gmail.com>
2020-05-26 23:22:39 -0400

    DELETE /api/meetings

--- server/meetings.js
+++ server/meetings.js
@@ -19,4 +19,9 @@ meetingsRouter.post('/', (req, res, next) => {
     res.status(201).send(newMeeting);
 });
 
+meetingsRouter.delete('/', (req, res, next) => {
+    deleteAllFromDatabase('meetings');
+    res.status(204).send();
+})
+
 module.exports = meetingsRouter;
