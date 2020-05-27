f80fcc298becdfaa8fad8b47225454d5648cdd25
Jacques Joky <jacques.joky@gmail.com>
2020-05-26 22:58:03 -0400

    checkMillionDollarIdea middleware

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,4 +1,12 @@
-const checkMillionDollarIdea = () => {};
+const checkMillionDollarIdea = (req, res, next) => {
+    const {weeklyRevenue, numWeeks} = req.body;
+    const totalValue = numWeeks * weeklyRevenue;
+    if (!numWeeks || !weeklyRevenue || isNaN(totalValue) || totalValue < 1000000) {
+        res.status(400).send();
+    } else {
+        next();
+    }
+};
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
--- server/ideas.js
+++ server/ideas.js
@@ -9,6 +9,7 @@ const {
     deleteFromDatabasebyId,
     deleteAllFromDatabase
 } = require('./db.js');
+const checkMillionDollarIdea = require('./checkMillionDollarIdea.js');
 
 ideasRouter.param('ideaId', (req, res, next, id) => {
     const idea = getFromDatabaseById('ideas', id);
@@ -28,12 +29,12 @@ ideasRouter.get('/:ideaId', (req, res, next) => {
     res.send(req.idea);
 });
 
-ideasRouter.put('/:ideaId', (req, res, next) => {
+ideasRouter.put('/:ideaId', checkMillionDollarIdea, (req, res, next) => {
     const updatedIdea = updateInstanceInDatabase('ideas', req.body);
     res.send(updatedIdea);
 });
 
-ideasRouter.post('/', (req, res, next) => {
+ideasRouter.post('/', checkMillionDollarIdea, (req, res, next) => {
     const newIdea = addToDatabase('ideas', req.body);
     res.status(201).send(newIdea);
 });
