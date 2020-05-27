a18494a2afe2e7fd53279f8f9e5eb78b2bb90b8a
Jacques Joky <jacques.joky@gmail.com>
2020-05-27 00:52:48 -0400

    PUT /api/minions/:minionId/work/:workId

--- server/work.js
+++ server/work.js
@@ -8,11 +8,30 @@ const {
     deleteFromDatabasebyId
 } = require('./db.js');
 
+workRouter.param('workId', (req, res, next, id) => {
+    const work = getFromDatabaseById('work', id);
+    if (work) {
+        req.work = work;
+        next();
+    } else {
+        res.status(404).send();
+    }
+});
+
 workRouter.get('/', (req, res, next) => {
     const work = getAllFromDatabase('work').filter(singleWork => {
         return singleWork.minionId === req.params.minionId;
     });
     res.send(work);
+});
+
+workRouter.put('/:workId', (req, res, next) => {
+    if (req.params.minionId === req.body.minionId) {
+        const updatedWork = updateInstanceInDatabase('work', req.body);
+        res.send(updatedWork);
+    } else {
+        res.status(400).send();
+    }
 });
 
 module.exports = workRouter;
